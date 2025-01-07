5029c2320d8d952f1ad6621d9923cdda1703f6f4
Lucas Goï <lucasgoi746@gmail.com>
2025-01-07 08:49:59 +0100

    Initial Commit

---
+++ Unsplash-API/Unsplash_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unsplash_APIApp.swift
+//  Unsplash-API
+//
+//  Created by Lucas Goï on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Unsplash_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
